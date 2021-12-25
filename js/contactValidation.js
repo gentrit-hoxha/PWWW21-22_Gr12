d23aca191db8f15264945665d6f2b829d19297a9
jetmirhalili11 <jetmirhalili11@yahoo.com>
2021-12-25 22:13:04 +0100

    added some functions in contactValidation

--- js/contactValidation.js
+++ js/contactValidation.js
@@ -21,3 +21,45 @@ signUp.addEventListener('submit', (e) =>{
 
 
 
+
+
+function checkMessage() {
+    let message_value = contactMessage.value;
+    
+    if (message_value === "") {
+        setErrorFor(contactMessage, "Ju lutem shenoni nje mesazh mos e lini te zbrazet"); 
+        }
+
+        else if ((message_value.length)<80) {
+            setErrorFor(contactMessage, "Mesazhi duhet te permbaje me shume se 30 karaktere");
+        }
+
+        else{
+            setSuccessFor(contactMessage, "Hapesira e mesazhit eshte ne rregyll");
+    }
+}
+
+
+
+function setErrorFor(input, message) {
+    var formControl = input;
+    formControl.className = 'error';
+
+
+    var small = formControl.nextElementSibling;
+    small.innerText = message;
+    small.className = 'errorMsg yes';
+}
+
+
+
+function setSuccessFor(input, message) {
+    const formControl = input;
+    formControl.className = 'success';
+
+
+
+     var small = formControl.nextElementSibling;
+     small.textContent = message;
+     small.className = 'successMsg yes';
+}
